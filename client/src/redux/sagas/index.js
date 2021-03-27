14e649fae74ac8ba600fc5c51f0f5e45cdae7c1e
Mitchell <mitchell.sung@gmail.com>
2021-03-26 18:09:48 -0600

    Install react.js and Set up Redux

---
+++ client/src/redux/sagas/index.js
@@ -0,0 +1,7 @@
+import {all} from 'redux-saga/effects';
+
+// The function* declaration defines a generator function, 
+// which returns a Generator object.
+export default function* rootSaga() {
+    yield all([]);
+};
