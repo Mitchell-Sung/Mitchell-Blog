750709ae16ed93e4aa7d463d24ab78e1befc620c
Mitchell <mitchell.sung@gmail.com>
2021-03-27 00:41:05 -0600

    Create Footer.js for Front-end

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {Row, Col} from 'reactstrap';
 
 const Footer = () => {
+    const thisYear = () => {
+        const year = new Date().getFullYear();
+        return year;
+    };
+
     return (
-        <div>
-            Footer
+        <div id='main-footer' className='text-center p-2'>
+            <Row>
+                <Col>
+                    <p>Copyright &copy;<span>{thisYear()}</span></p>
+                </Col>
+            </Row>
         </div>
     );
 };
 
 export default Footer;
+
+// Notes:
+// <Row> and <Col>???
+// Footer > Router.js > App.js > Index.js / how to find the reactstrap like row and col.
