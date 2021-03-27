f4528d43f8a4127f893e6a6678dd2b329c5186d0
Mitchell <mitchell.sung@gmail.com>
2021-03-27 02:44:08 -0600

    Create AppNavbar.js for front-end

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -1,15 +1,17 @@
 import React, { Fragment } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import AppNavbar from '../components/AppNavbar'; 
 
 const MyRouter = () => {
     return (
         <Fragment>
+            <AppNavbar/>
             <Header/>
                 <h1>Hello Body</h1>
             <Footer/>
         </Fragment>
     );
-}
+};
 
 export default MyRouter;
