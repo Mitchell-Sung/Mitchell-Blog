29cc824798c34c1b15de2e482a963a8e0b4ad51b
Mitchell <mitchell.sung@gmail.com>
2021-04-12 00:57:20 -0600

    Set up Post Route

--- server/models/category.js
+++ server/models/category.js
@@ -2,16 +2,18 @@ import mongoose, { mongo } from 'mongoose';
 
 // Create Schema
 const CategorySchema = new mongoose.Schema({
-    categoryName: {
-        type: String,
-        default:"undefined",
-    },
-    post: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "post",
-    },],
+	categoryName: {
+		type: String,
+		default: 'undefined',
+	},
+	posts: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'post',
+		},
+	],
 });
 
-const Category = mongoose.model("category", CategorySchema);
+const Category = mongoose.model('category', CategorySchema);
 
 export default Category;
--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -2,6 +2,8 @@ import express, { request, response } from 'express';
 import auth from '../../middleware/auth';
 // Model
 import Post from '../../models/post';
+import Category from '../../models/category';
+import User from '../../models/user';
 
 const router = express.Router();
 
@@ -11,6 +13,8 @@ import multerS3 from 'multer-s3'; // manage moving files with aws s3
 import path from 'path';
 import AWS from 'aws-sdk'; // support aws tool
 import dotenv from 'dotenv';
+import moment from 'moment';
+import { isNullOrUndefined } from 'util';
 
 dotenv.config();
 
@@ -60,18 +64,55 @@ router.get('/', async (request, response) => {
 	response.json(postFindResult); // server have to response to the browser.
 });
 
+// @route	POST api/post
+// @desc 	Create a Post
+// @access 	Private
+
 // Allow only authorized user to create posts.
-router.post('/', auth, async (request, response, next) => {
+// s40 Add
+router.post('/', auth, uploadS3.none(), async (request, response, next) => {
 	try {
 		console.log(request, 'request');
-		const { title, contents, fileUrl, creator } = request.body;
+		const { title, contents, fileUrl, creator, category } = request.body;
 		const newPost = await Post.create({
 			title: title,
 			contents: contents,
 			fileUrl: fileUrl,
 			creator: creator,
+			date: moment().format('YYYY-MM-DD hh:mm:ss'),
 		});
-		response.json(newPost);
+
+		const findResult = await Category.findOne({
+			categoryName: category,
+		});
+
+		console.log(findResult, 'Finde Result');
+
+		if (isNullOrUndefined(findResult)) {
+			const newCategory = await Category.create({
+				categoryName: category,
+			});
+			await Post.findByIdAndUpdate(newPost._id, {
+				$push: { category: newCategory._id },
+			});
+			await Category.findByIdAndUpdate(newCategory._id, {
+				$push: { posts: newPost._id },
+			});
+			await User.findByIdAndUpdate(req.user.id, {
+				$push: { posts: newPost._id },
+			});
+		} else {
+			await Category.findByIdAndUpdate(findResult._id, {
+				$push: { posts: newPost._id },
+			});
+			await Post.findByIdAndUpdate(newPost._id, {
+				category: findResult._id,
+			});
+			await User.findByIdAndUpdate(req.user.id, {
+				$push: { posts: newPost._id },
+			});
+		}
+		return res.redirect(`/api/post/${newPost._id}`);
 	} catch (err) {
 		console.log(err);
 	}
